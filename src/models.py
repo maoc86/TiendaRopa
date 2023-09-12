c55c030df1660e271e28de0b5ca25674d3587f2c
maoc86 <soymanuweb@gmail.com>
2023-09-12 18:23:36 +0000

    PRIMER COMMIT

--- src/models.py
+++ src/models.py
@@ -4,8 +4,11 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    apellido = db.Column(db.String(120), unique=False, nullable=False)
     is_active = db.Column(db.Boolean(), unique=False, nullable=False)
 
     def __repr__(self):
@@ -15,5 +18,43 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "nombre": self.nombre,
+            "apellido": self.apellido
             # do not serialize the password, its a security breach
         }
+
+    class Cliente(db.Model):
+        __tablename__ = "cliente"
+        id = db.Column(db.Integer, primary_key=True)
+        nombre = db.Column(db.String(120), unique=True, nullable=False)
+        apellido = db.Column(db.String(120), unique=True, nullable=False)
+        documento = db.Column(db.Integer, unique=True, nullable=False)
+        direccion = db.Column(db.String(120), unique=True, nullable=False)
+        email = db.Column(db.String(80), unique=False, nullable=False)
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "apellido": self.apellido,
+            "documento": self.documento,
+            "direccion": self.direccion,
+            "email": self.email
+            }
+
+    class Producto(db.Model):
+        __tablename__ = "producto"
+        id = db.Column(db.Integer, primary_key=True)
+        nombre = db.Column(db.String(120), unique=True, nullable=False)
+        descripcion = db.Column(db.String(120), unique=True, nullable=False)
+        talla = db.Column(db.String(80), unique=True, nullable=False)
+        precio = db.Column(db.Integer, unique=True, nullable=False)
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "nombre": self.nombre,
+            "descripcion": self.apellido,
+            "talla": self.documento,
+            "precio": self.direccion
+                        }
